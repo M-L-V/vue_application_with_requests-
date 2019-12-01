83dc411373088584fa8018148724ec303c29b7f8
Morgaine <morgainedv@gmail.com>
2019-12-01 16:31:23 +0000

    finished MVP

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template lang="html">
   <div>
-
     <h1> Studio Ghibli characters API </h1>
     <div>
       <characters-list :characters='characters'></characters-list>
-      <selected-character :character='selectedCharacter'></selected-character>
-
+      <character-detail :character='selectedCharacter'></character-detail>
     </div>
-
-
   </div>
 
 </template>
 
+
 <script>
 
+import { eventBus } from './main.js'
 import CharactersList from './components/CharactersList.vue'
+import CharacterDetail from './components/CharacterDetail.vue'
 
 
 export default {
@@ -26,35 +25,20 @@ export default {
   },
   components: {
     "characters-list": CharactersList,
-    // "character-detail": CharacterDetail
+    "character-detail": CharacterDetail
   },
   mounted(){
     fetch('https://ghibliapi.herokuapp.com/people')
     .then(res => res.json())
     .then(characters => this.characters = characters)
 
-
+    eventBus.$on('character-selected', (character) => {
+      this.selectedCharacter = character
+    })
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 </script>
-
-
-
-
 
 
 
--- src/components/CharacterDetail.vue
+++ src/components/CharacterDetail.vue
@@ -1,17 +1,23 @@
 <template lang="html">
 
+<div v-if='character'>
+  <h1> {{character.name}} </h1>
+  <p> Age: {{character.age}} </p>
+  <p> Gender: {{character.gender}} </p>
+
+</div>
 
 
 
 </template>
 
 
-
-
-
 <script>
 export default {
+  name: 'character-detail',
+  props: ['character']
 }
+
 </script>
 
 
